68cb36007784acdf7297407c8db9966174d4c976
EmilFerdinanA <emilakbar55@gmail.com>
2025-08-07 13:21:43 +0700

    create role

--- app/(main)/role/create/page.tsx
+++ app/(main)/role/create/page.tsx
@@ -10,8 +10,14 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 export default function page() {
+  const tags = Array.from({ length: 50 }).map(
+    (_, i, a) => `v1.2.0-beta.${a.length - i}`
+  );
+
   return (
     <Card className="w-full">
       <CardHeader>
@@ -40,6 +46,44 @@ export default function page() {
               <Input id="password" type="password" required />
             </div>
           </div>
+
+          <Card className="mt-6 w-1/2">
+            <CardContent className="flex justify-around">
+              <div>
+                <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+                <Input
+                  // value={query}
+                  placeholder="Filter names..."
+                  // onChange={(event) => setQuery(event.target.value)}
+                  className="max-w-sm"
+                />
+                <ScrollArea className="h-72 w-full rounded-md border">
+                  <div className="p-4">
+                    {tags.map((tag) => (
+                      <React.Fragment key={tag}>
+                        <div className="text-sm">{tag}</div>
+                        <Separator className="my-2" />
+                      </React.Fragment>
+                    ))}
+                  </div>
+                </ScrollArea>
+              </div>
+
+              <div>
+                <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
+                <ScrollArea className="h-72 w-48 rounded-md border">
+                  <div className="p-4">
+                    {["emil"].map((tag) => (
+                      <React.Fragment key={tag}>
+                        <div className="text-sm">{tag}</div>
+                        <Separator className="my-2" />
+                      </React.Fragment>
+                    ))}
+                  </div>
+                </ScrollArea>
+              </div>
+            </CardContent>
+          </Card>
         </form>
       </CardContent>
 
