c9243b1058c726d6d5f34b8c215a2f8b69477fe2
EmilFerdinanA <emilakbar55@gmail.com>
2025-08-07 13:38:13 +0700

    add dashboard to sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,14 +3,12 @@
 import * as React from "react";
 import {
   AudioWaveform,
-  BookOpen,
-  Bot,
   Command,
   Frame,
   GalleryVerticalEnd,
+  LayoutDashboard,
   Map,
   PieChart,
-  Settings2,
   Users,
 } from "lucide-react";
 
@@ -52,10 +50,15 @@ const data = {
   ],
   navMain: [
     {
+      title: "Dashboard",
+      url: "",
+      icon: LayoutDashboard,
+    },
+    {
       title: "User Management",
       url: "",
       icon: Users,
-      isActive: true,
+      isActive: false,
       items: [
         {
           title: "User",
@@ -67,71 +70,6 @@ const data = {
         },
         {
           title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
           url: "#",
         },
       ],
